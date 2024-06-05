64afe04cce28cbccee4a66f3c60df4425d12af26
Prince2802 <chaudhary.prince97@gmail.com>
2024-06-05 19:54:07 +0530

    .

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./User";
+
 const About = () => {
     return(
         <div>
             <h1>About Us</h1>
             <h2>Created by : Prince Chaudhary</h2>
+            <User />
         </div>
     );
 };
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+    <div className="user-card">
+        <div>Name: Prince</div>
+        <div>Location: Delhi</div>
+        <div>Contact: chaudhary.prince97@gmail.com</div>
+    </div>
+    )
+}
+
+export default User;
