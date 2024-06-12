a158a4186b6f1b769ecde3a4906e7666160fd32d
Prince2802 <chaudhary.prince97@gmail.com>
2024-06-12 22:35:06 +0530

    .

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+
 const User = () => {
-    return (
+
+  const [user,setUser]= useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://api.github.com/users/Prince2802");
+    const json = await data.json();
+    console.log(json);
+    setUser(json);
+  };
+
+  const{name,location,company} = setUser;
+
+  return (
     <div className="user-card">
-        <div>Name: Prince</div>
-        <div>Location: Delhi</div>
-        <div>Contact: chaudhary.prince97@gmail.com</div>
+      <div>name : {name}</div>
+      <div>location :{location}</div>
+      <div>company : {company}</div>
     </div>
-    )
-}
+  );
+};
 
 export default User;
